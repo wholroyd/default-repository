9a62f8427a9178cc606fd646fe949eba28f58698
William Holroyd <wholroyd@gmail.com>
2015-09-12 23:11:09 -0400

    Still wip, colors and input are done

--- actions.sh
+++ actions.sh
@@ -4,15 +4,14 @@ function do_build {
 
    current=`pwd`
 
-   echo
-   echo Starting package restoration...
+   printf "\n${header}Starting package restoration...${nc}"
 
    # restore the nuget packages
    for dir in $(find -name 'project.json' -printf '%h\n' | sort -u)
    do
       cd $current
       cd $dir
-      echo Restoring for DNX in $(pwd)
+      echo -Restoring for DNX in $(pwd)
       dnx . restore
    done
    cd $current
@@ -22,7 +21,7 @@ function do_build {
    do
       cd $current
       cd $dir
-      echo Restoring for NPM in $(pwd)
+      echo -Restoring with NPM in $(pwd)
       npm install
       npm install -g bower
       npm install -g gulp
@@ -34,136 +33,144 @@ function do_build {
    do
       cd $current
       cd $dir
-      echo Restoring for Bower in $(pwd)
+      echo -Restoring with Bower in $(pwd)
       bower install
    done
    cd $current
 
-   echo
-   echo Building source...
+   printf "\n${header}Building source...${nc}\n"
 
     # build using gulp
    for dir in $(find -name 'gulpfile.js' -printf '%h\n' | sort -u)
    do
       cd $current
       cd $dir
-      echo Building for Gulp in $(pwd)
+      echo -Building with Gulp in $(pwd)
       gulp default
    done
    cd $current
 
-   echo
-   echo Completed build phase
-   echo
+   printf "\n${green}Completed build phase${nc}\n"
 }
 
 function do_test {
-   echo Test
+   printf Test
 
 }
 
-function do_docker_create {
+function do_create {
+
+  printf "\n${header}Starting Docker image creation...${nc}\n"
 
   # We expect that when this script is ran for docker image creation that
   # there are some particular settings set prior to starting, or are given
   # on the command line when invoking it.
 
-  # Args       $1     $2         $3
-  # actions.sh create {teamname} {reponame}
+  # We require {team} {repo}, or take from git
+  verify_repository
 
-  echo
-  echo Starting Docker image creation...
-
-  if [ -n "$DOCKER_TEAM" ]
-  then
-    echo Team name used from environment DOCKER_TEAM variable...
-  else
-    if [ -n "$2" ]
-    then
-      echo Team name used from script parameter...
-      DOCKER_TEAM=$2
-    else
-      # This assumes the Docker team name is the same as the Git team/user name
-      echo Team name used from git...
-      DOCKER_TEAM=`git remote show origin | grep "Fetch URL:" | sed "s#^.*/\(.*\)/\(.*\).git#\1#"`
-    fi
-  fi
-
-  if [ -n "$DOCKER_REPO" ]
-  then
-    echo Repo name used from environment DOCKER_REPO variable...
-  else
-    if [ -n "$3" ]
-    then
-      echo Repo name used from script parameter...
-      DOCKER_REPO=$3
-    else
-      # This assumes the Docker repo name is the same as the Git repo name
-      echo Repo name used from git...
-      DOCKER_REPO=`git remote show origin | grep "Fetch URL:" | sed "s#^.*/\(.*\)/\(.*\).git#\2#"`
-    fi
-  fi
-
-  echo
-  echo Starting image creation...
-
+  printf "\n${header}Starting image creation...${nc}\n"
   docker build -t $DOCKER_TEAM/$DOCKER_REPO:build .
 
-  echo
-  echo Starting image optimization/layer-merging...
+  printf "\n${header}Starting image optimization/layer-merging...${nc}\n"
 
-  # Tagged by 'latest'
+  # Create image tagged 'latest'
   ID=$(docker run -d $DOCKER_TEAM/$DOCKER_REPO:build /bin/bash)
   docker export $ID | docker import - $DOCKER_TEAM/$DOCKER_REPO:latest
 
-  # Tagged by git commit
+  # Create image tagged by git commit id
   TAG=$(git rev-parse --short HEAD)
   docker export $ID | docker import - $DOCKER_TEAM/$DOCKER_REPO:$TAG
 
-  echo
-  echo Starting image and container cleanup...
-
+  printf "\n${header}Starting image and container cleanup...${nc}"
   docker rm $(docker ps -l -q)
   docker rmi -f `docker images $DOCKER_TEAM/$DOCKER_REPO | grep "build" | awk 'BEGIN{FS=OFS=" "}{print $3}'`
 
-  echo
-  echo Completed creation phase
-  echo
+  printf "\n${green}Completed creation phase${nc}\n\n"
 }
 
-function do_docker_deploy {
+function do_deploy {
 
-  echo
-  echo Starting Docker image deployment...
+  printf "\n${header}Starting Docker image deployment...${nc}"
 
-  echo
-  echo Registry authentication...
+  # We expect that when this script is ran for docker image deployment that
+  # there are some particular settings set prior to starting, or are given
+  # on the command line when invoking it.
 
+  # We require {team} {repo}, or take from git
+  verify_repository
+
+  # We require {username} {password} {email}, or fail
+  verify_authentication
+
+  printf "\nRegistry authentication..."
   docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD -e $DOCKER_EMAIL
 
-  echo
-  echo Registry upload...
-
+  printf "\nRegistry upload..."
   docker push -f $DOCKER_TEAM/$DOCKER_REPO:latest
 
-  echo
-  echo Completed deploy phase
-  echo
+  printf "\n${green}Completed deploy phase${nc}\n\n"
 }
 
 function do_universe {
-  do_compile
+  do_build
   do_test
-  do_docker_create
-  do_docker_deploy
+  do_create
+  do_deploy
 }
 
 function do_galaxy {
-  do_compile
+  do_build
   do_test
-  do_docker_create
+  do_create
 }
 
+function verify_repository {
+  if [ -n "$DOCKER_TEAM" ] && [ -n "$DOCKER_REPO" ]
+  then
+    echo -Using environment variables
+  else
+    if [ -n "$2" ] && [ -n "$3" ]
+    then
+      echo -Using script parameters
+      DOCKER_TEAM=$2
+      DOCKER_REPO=$3
+    else
+      echo -Using git information
+      DOCKER_TEAM=`git remote show origin | grep "Fetch URL:" | sed "s#^.*/\(.*\)/\(.*\).git#\1#"`
+      DOCKER_REPO=`git remote show origin | grep "Fetch URL:" | sed "s#^.*/\(.*\)/\(.*\).git#\2#"`
+    fi
+  fi
+}
+
+function verify_authentication {
+  if [ -n "$DOCKER_USERNAME" ] && [ -n "$DOCKER_PASSWORD" ] && [ -n "$DOCKER_EMAIL" ]
+  then
+    echo -Using environment variables
+  else
+    if [ -n "$4" ] && [ -n "$5" ] && [ -n "$6" ]
+    then
+      echo -Using script parameters
+      DOCKER_USERNAME=$4
+      DOCKER_PASSWORD=$5
+      DOCKER_EMAIL=$6
+    else
+      printf "\n-Unable to continue, the following values were not provided:"
+      printf "\n\tDOCKER_USERNAME\n\tDOCKER_PASSWORD\n\tDOCKER_EMAIL\n"
+
+      printf "\n${red}Failed deploy phase${nc}\n\n"
+      exit 1
+    fi
+  fi
+}
+
+# Console coloring
+red='\033[0;31m'
+green='\033[0;32m'
+header='\033[0;34m'
+nc='\033[0m' # No Color
+
+# Determine what needs to be ran
 case $1 in
 build)
   do_build
@@ -172,10 +179,10 @@ test)
   do_test
   ;;
 create)
-  do_docker_create
+  do_create
   ;;
 deploy)
-  do_docker_deploy
+  do_deploy
   ;;
 universe)
   do_universe
@@ -184,17 +191,17 @@ galaxy)
   do_galaxy
   ;;
 *)
-  echo ""
-  echo "Usage: $0 [OPTION]"
-  echo "  OPTION            Performs..."
-  echo "  ----------------  ----------------------------------------------------------"
-  echo "  build             ...a compilation, if required"
-  echo "  test              ...all tests in order of unit, integration, and functional"
-  echo "  create            ...a Docker image"
-  echo "  deploy            ...a Docker image"
-  echo "  "
-  echo "  universe          ...in order the options: compile, test, docker, deploy"
-  echo "  galaxy            ...in order the options: compile, test, docker"
-  echo "  "
+  printf "\n"
+  printf "Usage: $0 [OPTION]\n"
+  printf "  OPTION            Performs...\n"
+  printf "  ----------------  ----------------------------------------------------------\n"
+  printf "  build             ...a compilation, if required\n"
+  printf "  test              ...all tests in order of unit, integration, and functional\n"
+  printf "  create            ...a Docker image\n"
+  printf "  deploy            ...a Docker image\n"
+  printf "\n"
+  printf "  universe          ...in order the options: build, test, create, deploy\n"
+  printf "  galaxy            ...in order the options: build, test, create\n"
+  printf "\n"
   ;;
 esac
